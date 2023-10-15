d39c14bd92ea35e709144dcac4ff502a89481796
Daniel Dietzler <mail@ddietzler.dev>
2023-10-15 19:04:09 +0200

    fix circular dependencies

--- server/src/domain/system-config/dto/system-config.dto.ts
+++ server/src/domain/system-config/dto/system-config.dto.ts
@@ -1,15 +1,17 @@
-import { SystemConfigLibraryScanDto, SystemConfigThumbnailDto, SystemConfigTrashDto } from '@app/domain/system-config';
 import { SystemConfig } from '@app/infra/entities';
 import { Type } from 'class-transformer';
 import { IsObject, ValidateNested } from 'class-validator';
 import { SystemConfigFFmpegDto } from './system-config-ffmpeg.dto';
 import { SystemConfigJobDto } from './system-config-job.dto';
+import { SystemConfigLibraryScanDto } from './system-config-library-scan.dto';
 import { SystemConfigMachineLearningDto } from './system-config-machine-learning.dto';
 import { SystemConfigMapDto } from './system-config-map.dto';
 import { SystemConfigOAuthDto } from './system-config-oauth.dto';
 import { SystemConfigPasswordLoginDto } from './system-config-password-login.dto';
 import { SystemConfigReverseGeocodingDto } from './system-config-reverse-geocoding.dto';
 import { SystemConfigStorageTemplateDto } from './system-config-storage-template.dto';
+import { SystemConfigThumbnailDto } from './system-config-thumbnail.dto';
+import { SystemConfigTrashDto } from './system-config-trash.dto';
 
 export class SystemConfigDto implements SystemConfig {
   @Type(() => SystemConfigFFmpegDto)
